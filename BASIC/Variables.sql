9891d7bf3ed5357bc8ab6ba31fd8ee75080f16e3
BRINDHA M D <mdbrindha242@gmail.com>
2026-06-27 04:31:56 -0700

    Create Variables.sql

---
+++ BASIC/Variables.sql
@@ -0,0 +1,69 @@
+# variables - numeric variables
+age = 25
+print(age)
+ans:25
+
+# string variables
+name = 'bindhu'
+print(name)
+ans:bindhu
+
+# boolean variables 
+is_true = 'True'
+print(is_true)
+# list variables
+numbers = [1,2,3,4]
+names = ['swetha','ramya','amutha']
+print(names)
+#tuple variables
+axes = ('x','y')
+print(axes)
+#dictionary variables
+person = {name : 'asha',
+         age : 22}
+print(age)
+#set variables
+unique_num = {1,2,3}
+print(unique_num)
+ans:True
+['swetha', 'ramya', 'amutha']
+('x', 'y')
+25
+{1, 2, 3}
+
+
+num = 20
+print(num)
+ans:20
+
+# type conversion
+x =1
+y = 5.1
+z = int(y)
+print(z,type(z))
+ans:5 <class 'int'>
+
+x = '123'
+y= int(x)
+print(y,type(y))
+ans:123 <class 'int'>
+
+Y=2.5
+Z=int(y)
+print(z,type(z))
+#
+y = 6
+z=float(y)
+print(z,type(z))
+x=True
+y=int(x)
+print(y,type(y))
+#
+x=[1,2,3]
+y=list(map(str,x))
+print(y,type(y))
+ans:5 <class 'int'>
+6.0 <class 'float'>
+1 <class 'int'>
+['1', '2', '3'] <class 'list'>
+
