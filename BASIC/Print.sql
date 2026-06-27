2cb33e53013e6a29455c5e0c8c01b60ccfe59346
BRINDHA M D <mdbrindha242@gmail.com>
2026-06-27 04:24:55 -0700

    Create Print.sql

---
+++ BASIC/Print.sql
@@ -0,0 +1,47 @@
+# to use print func
+print("I love python")
+ans:I love python
+
+print(12)
+ans:12
+
+print(1+2)
+ans:3
+
+print('1+2')
+ans:'1+2'
+
+print(99)
+print("i love")
+print(1+2)
+ans:99
+i love
+3
+
+# multiple print statement
+print(3+4)
+print(2+1)
+ans:7
+3
+
+# print in many item in single print
+print(1,2,3,4)
+ans:1 2 3 4
+
+print(1, 2, 3, 4)
+ans:1 2 3 4
+
+print(3+4,2+1)
+ans:7 3
+
+print('3+4,2+1')
+ans:3+4,2+1
+
+# string and num in print
+print("my number is ", 4)
+ans:my number is  4
+
+print(7,'plus',3,'equals',10)
+ans:7 plus 3 equals 10
+
+
