615f813e9c25fd9dff93f57d3577f43b38a1afc0
BRINDHA M D <mdbrindha242@gmail.com>
2026-06-27 04:40:47 -0700

    Create Operators.sql

---
+++ BASIC/Operators.sql
@@ -0,0 +1,72 @@
+#ARITHMETIC OPERATORS
+x = 5
+y = 10
+z = (x + y)*2
+print(z)
+#COMPARISON
+a = 5
+b = 3
+print(a > b and b<a)
+#ASSIGNMENT
+z = 20
+z +=5
+z //=3
+print(z)
+#LOGICAL
+x= True
+y=False
+print(x and y)
+#BITWISE
+a = 10
+b=4
+z=a&b
+print(z)
+#MEMBERSHIP
+list = [1,2,3,4,5]
+print(5 in list)
+print(6 in list)
+#identity
+x=[1,2,3]
+y=[1,2,3,4]
+print(x is not y)
+print(x==y) 
+  ans:30
+True
+8
+False
+0
+True
+False
+True
+False
+
+a = 23
+b = 20
+print(a +b)
+ ans:43
+
+l=45
+w=76
+a = l*w
+print(a)
+ ans:3420
+
+r=8.9
+p=3.14
+print(p*r*r)
+ ans:248.71940000000004
+
+a='learning'
+b=" is fun!"
+print(a+b)
+ ans:learning is fun!
+
+
+temp = 25.5
+celsius = temp
+kelvin=273 + temp
+print('celsius:',celsius)
+print('kelvin:',kelvin)
+ ans:celsius: 25.5
+     kelvin: 298.5
+
