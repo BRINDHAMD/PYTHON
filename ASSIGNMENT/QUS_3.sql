0ddbbf7c934bdcb1a0ea8aba502e61ab69adedbf
BRINDHA M D <mdbrindha242@gmail.com>
2026-06-29 02:55:55 -0700

    Create QUS_3.sql

---
+++ ASSIGNMENT/QUS_3.sql
@@ -0,0 +1,8 @@
+#Write a script that takes a user's name as input and prints a personalized greeting (e.g., "Hello, Alex!").
+name = input('enter your name:')
+greet = 'Good Morning,'
+name_greet = greet +' '+ name
+print(name_greet) 
+
+ANS:enter your name: noyal
+Good Morning, noyal
