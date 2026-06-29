b53d6c1be0900e72ede9992b6ddf9af1c0052d45
BRINDHA M D <mdbrindha242@gmail.com>
2026-06-29 03:07:47 -0700

    Create QUS_4.sql

---
+++ ASSIGNMENT/QUS_4.sql
@@ -0,0 +1,8 @@
+#Ask the user for a temperature in Celsius and convert it to Fahrenheit.
+temp = int(input('Enter a value:')) 
+F = (temp*1.8)+32
+print('the Fahrenheit is:',F)
+
+ANS:
+Enter a value: 23
+the Fahrenheit is: 73.4
