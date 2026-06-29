0c2829f5f1ed563a47c9808405ca44b2b04a8b4c
BRINDHA M D <mdbrindha242@gmail.com>
2026-06-29 03:50:38 -0700

    Create QUS_7.sql

---
+++ ASSIGNMENT/QUS_7.sql
@@ -0,0 +1,10 @@
+#Create a script that takes an age as input and checks if the person is eligible to vote (18 or older).
+  age = int(input('enter your age:'))
+if (age >= 18):
+    print('eligible to vote')
+
+else:
+ print('not eligible to vote')
+
+ANS:enter your age: 12
+not eligible to vote
