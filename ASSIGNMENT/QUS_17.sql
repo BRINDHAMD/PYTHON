0fe0eddd98e89b2a74f51720e619fcc9f823a150
BRINDHA M D <mdbrindha242@gmail.com>
2026-06-30 04:04:19 -0700

    Create QUS_17.sql

---
+++ ASSIGNMENT/QUS_17.sql
@@ -0,0 +1,9 @@
+#Append a new fruit to your list, remove the second fruit, and print the updated list.
+my_list = ['apple','banana','cherry','grapes','orange']
+my_list.append('fig')
+my_list.remove('banana')
+my_list
+
+ANS:
+['apple', 'cherry', 'grapes', 'orange', 'fig']
+  
