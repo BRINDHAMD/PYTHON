53316b28695dbdcd77a2548a6c24d64ff3b36997
BRINDHA M D <mdbrindha242@gmail.com>
2026-06-30 03:46:51 -0700

    Create QUS_14.sql

---
+++ ASSIGNMENT/QUS_14.sql
@@ -0,0 +1,21 @@
+#Print the multiplication table of a given number.
+ n = 5
+print('multiplication table of 5:\n')
+for i in range(1,11):
+    mul = i*5
+    
+    print( i,'*5=',mul)
+
+ANS:
+multiplication table of 5:
+
+1 *5= 5
+2 *5= 10
+3 *5= 15
+4 *5= 20
+5 *5= 25
+6 *5= 30
+7 *5= 35
+8 *5= 40
+9 *5= 45
+10 *5= 50
