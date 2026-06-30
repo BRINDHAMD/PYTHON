a217da2a45f30ca2aaacb2508fc70e903aec1802
BRINDHA M D <mdbrindha242@gmail.com>
2026-06-30 03:27:30 -0700

    Add while loop to print numbers from 10 to 1
    Added a script to print numbers from 10 to 1 using a while loop.

---
+++ ASSIGNMENT/QUS_12.sql
@@ -0,0 +1,19 @@
+#Print the numbers from 10 down to 1 using a while loop.
+  num = 10
+while num <= 10:
+    if num ==0:
+        break
+    print(num)
+    num-=1
+
+ANS:
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
