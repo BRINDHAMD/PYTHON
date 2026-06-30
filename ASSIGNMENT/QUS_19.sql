65c724cf4ce2299702e6d9d4cb0bb10020038399
BRINDHA M D <mdbrindha242@gmail.com>
2026-06-30 04:15:09 -0700

    Create QUS_19.sql

---
+++ ASSIGNMENT/QUS_19.sql
@@ -0,0 +1,7 @@
+#Check if a specific element exists in a list (e.g., check if 'apple' is in your fruit list).
+  my_list = ['apple','banana','cherry','grapes','orange']
+if 'apple' in my_list:
+    print('yes')
+
+ANS:
+yes
