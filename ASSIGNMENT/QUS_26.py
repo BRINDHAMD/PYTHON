d06cba2b59775756856942164a5d454568618b9e
BRINDHA M D <mdbrindha242@gmail.com>
2026-07-01 05:32:08 -0700

    Create QUS_26.py

---
+++ ASSIGNMENT/QUS_26.py
@@ -0,0 +1,12 @@
+#Find the largest and smallest numbers in a given list.
+my_list = [11,34,78,66,45]
+for n in my_list:
+    max_num = max(my_list)
+    min_num = min(my_list)
+
+print('the maximum number is:',max_num)
+print('the minimum number is:',min_num)
+
+ANS:
+the maximum number is: 78
+the minimum number is: 11
