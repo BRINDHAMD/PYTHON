1fdefd6ba150063b113387f75cd5f47ece7e047c
BRINDHA M D <mdbrindha242@gmail.com>
2026-07-01 05:21:14 -0700

    Create QUS_24.py

---
+++ ASSIGNMENT/QUS_24.py
@@ -0,0 +1,10 @@
+#Write a function max_of_two(x, y) that returns the larger number without using Python's built-in max() function.
+def max_num(a,b):
+    if a > b:
+        print('the greatest number is:',a)
+    else:
+        print('the greatest number is:',b)
+max_num(58,75)
+
+ANS:
+the greatest number is: 75
