a5407ee0ff46b32c3e41a89d06668163e7ad9951
BRINDHA M D <mdbrindha242@gmail.com>
2026-07-01 05:58:31 -0700

    Create QUS_28.py

---
+++ ASSIGNMENT/QUS_28.py
@@ -0,0 +1,10 @@
+#Create a dictionary representing a person (name, age, city). Print only the keys, then print only the values.
+my_dict = {'name' : 'charu',
+          'age' : 22,
+          'city' : 'chennai'}
+print(my_dict.keys())
+print(my_dict.values())
+
+ANS:
+dict_keys(['name', 'age', 'city'])
+dict_values(['charu', 22, 'chennai'])
