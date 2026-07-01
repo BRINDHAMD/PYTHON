627ed56e103ab89f6e37e4a8a7f2504b5447772f
BRINDHA M D <mdbrindha242@gmail.com>
2026-07-01 06:01:54 -0700

    Create QUS_29.py

---
+++ ASSIGNMENT/QUS_29.py
@@ -0,0 +1,9 @@
+#Add a new key-value pair to the dictionary and update an existing one.
+my_dict = {'name' : 'charu',
+          'age' : 22,
+          'city' : 'chennai'}
+my_dict.update({'year' : 2002})
+print(my_dict)
+
+ANS:
+{'name': 'charu', 'age': 22, 'city': 'chennai', 'year': 2002}
