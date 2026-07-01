397d1e2070ca606b8d789718503ed3f68df76366
BRINDHA M D <mdbrindha242@gmail.com>
2026-07-01 06:04:20 -0700

    Create QUS_30.py

---
+++ ASSIGNMENT/QUS_30.py
@@ -0,0 +1,8 @@
+#Given two lists, combine them into a single sorted list.
+list1 = ['apple','banana','cherry']
+list2 = ['grapes','mango']
+my_list = list1 + list2
+print(my_list)
+
+ANS:
+['apple', 'banana', 'cherry', 'grapes', 'mango']
