26284bdbee61e93d63c6c4d5db50c9849f070a49
BRINDHA M D <mdbrindha242@gmail.com>
2026-07-01 06:11:12 -0700

    Create QUS_31.py

---
+++ ASSIGNMENT/QUS_31.py
@@ -0,0 +1,9 @@
+#Check if a given string is a palindrome (reads the same backward as forward, like "radar").
+name = 'madam'
+if name == name[::-1]:
+    print('the string is palindrome')
+else:
+    print('the string is not palindrome')
+
+ANS:
+the string is palindrome
