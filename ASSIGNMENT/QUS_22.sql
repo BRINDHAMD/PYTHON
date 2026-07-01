48bd4ed3a2cbbc4a70df626b68290b5b0b5a9150
BRINDHA M D <mdbrindha242@gmail.com>
2026-07-01 04:58:27 -0700

    Create QUS_22.sql

---
+++ ASSIGNMENT/QUS_22.sql
@@ -0,0 +1,8 @@
+#Write a function that takes a radius and returns the area of a circle.
+  def area(r):
+   return r*r
+result = area(7)
+print('the area of circle:',result)
+
+ANS:
+the area of circle: 49
