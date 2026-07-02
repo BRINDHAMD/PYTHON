c3a86e1cb1f77b4233b7fb0ba549137d97887603
BRINDHA M D <mdbrindha242@gmail.com>
2026-07-02 09:09:17 -0700

    Create QUS_42.py

---
+++ ASSIGNMENT/QUS_42.py
@@ -0,0 +1,6 @@
+#Use a dictionary comprehension to create a dictionary where keys are numbers from 1 to 5 and values are their cubes.
+dict = {i:i*i for i in range(1,6)}
+print(dict)
+
+ANS:
+{1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
