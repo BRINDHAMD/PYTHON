61b4a7561262b74ac08b975c1b8d16c44bee18d3
BRINDHA M D <mdbrindha242@gmail.com>
2026-07-02 06:18:35 -0700

    Add script to join words into a hyphenated string

---
+++ ASSIGNMENT/QUS_35.py
@@ -0,0 +1,7 @@
+#Join a list of words into a single sentence string using a hyphen (-) as a separator.
+word = ['the', 'greatest', 'of', 'all', 'time']
+s = '-'.join(word)
+print(s)
+
+ANS:
+the-greatest-of-all-time
