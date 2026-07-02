ca92489f8377d5a88f0381213d770aa60275fe39
BRINDHA M D <mdbrindha242@gmail.com>
2026-07-02 06:27:46 -0700

    Create QUS_36.py

---
+++ ASSIGNMENT/QUS_36.py
@@ -0,0 +1,10 @@
+#Write a program to generate the Fibonacci sequence up to n terms.
+n = int(input('enter a number:'))
+fibno = 0
+for i in range(0,n+1):
+    fibno+=i
+print('the fibno sequences is:',fibno)
+
+ANS:
+enter a number: 6
+the fibno sequences is: 21
