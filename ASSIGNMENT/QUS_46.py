3daec4ff729e3002a719e969a9dc48ca3970e8da
BRINDHA M D <mdbrindha242@gmail.com>
2026-07-02 09:40:52 -0700

    Create QUS_46.py

---
+++ ASSIGNMENT/QUS_46.py
@@ -0,0 +1,7 @@
+#Write a program that counts the total number of words in a text file.
+with open(r'C:\Users\Dell\Desktop\notes.txt', 'r') as file:
+    content = file.read()
+    print(content)
+
+ANS:
+First line
