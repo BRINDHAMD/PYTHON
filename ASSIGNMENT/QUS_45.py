abe845aa2dcc93681bdad186cc2352ca0c2e7292
BRINDHA M D <mdbrindha242@gmail.com>
2026-07-02 09:38:37 -0700

    Create QUS_45.py

---
+++ ASSIGNMENT/QUS_45.py
@@ -0,0 +1,9 @@
+#Write a script that creates a text file named notes.txt and writes three lines of text into it.
+file = open(r'C:\Users\Dell\Desktop\notes.txt', 'w')
+
+file.write("First line\n")
+
+file.close()
+
+ANS:
+First line
