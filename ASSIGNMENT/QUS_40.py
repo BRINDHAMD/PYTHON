ce0fe23921ad932c6b5331dc9e452828dd87b2f6
BRINDHA M D <mdbrindha242@gmail.com>
2026-07-02 09:00:58 -0700

    Create QUS_40.py

---
+++ ASSIGNMENT/QUS_40.py
@@ -0,0 +1,10 @@
+#Check if two strings are anagrams of each other (e.g., "listen" and "silent").
+w1 = 'listen'
+w2 = 'silent'
+if len(w1) != len(w2):
+    print('it is not anagrams')
+else:
+    print('it is  anagram')
+
+ANS:
+it is  anagram
