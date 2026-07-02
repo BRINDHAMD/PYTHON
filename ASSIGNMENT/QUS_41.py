ec05ff7477ac344d84ef251069ff3e86227a1768
BRINDHA M D <mdbrindha242@gmail.com>
2026-07-02 09:06:03 -0700

    Create QUS_41.py

---
+++ ASSIGNMENT/QUS_41.py
@@ -0,0 +1,6 @@
+#Use a list comprehension to create a list of squares of even numbers from 1 to 20
+num = [i*i for i in range(1,21)]
+print(num)
+
+ANS:
+[1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400]
